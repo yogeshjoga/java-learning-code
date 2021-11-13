be07cbb30571af7a4c8d2b02496e4f768f0a1aaf
yogesh joga <yjoga27@gmail.com>
2021-11-13 19:38:02 +0530

    java

---
+++ Telugu.java
@@ -0,0 +1,12 @@
+public class Telugu {
+     char jname = 'U+0C2F';
+
+//UNI CODE ESCAPE SEQUENCE IS ACCEPETED
+//UNI CODES NOT ACCEPTED 
+//WHY
+public static void main(String[]args){
+    System.out.println("telugu word" + jname );
+    
+
+}  
+}
