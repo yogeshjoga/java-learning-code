b1c7f5c2fd0216325b9f4b87ebc3901f00112424
yogesh joga <yjoga27@gmail.com>
2021-11-18 23:25:02 +0530

    patterns

---
+++ patterns/demo.java
@@ -0,0 +1,29 @@
+//pattern printing demo 
+class demo{
+    //int a, b, c;
+    static void stars(){
+        int a, b, c;
+          for(a=0; a<100; a++ )
+          {
+              for(b=0; b<=a; b++){    
+                System.out.println("*");
+              }
+          }
+    }
+    public static void main(String[] args){
+    System.out.println("patterns");
+    stars();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
