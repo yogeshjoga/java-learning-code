836ee13600a556e3c3617bfdf1ed0e92d1d20005
yogesh joga <yjoga27@gmail.com>
2021-11-21 19:19:10 +0530

    project

--- patterns/y.java
+++
@@ -1,8 +0,0 @@
-// stars to print y letter 
-// lets start the programming
-class y {
-    static void y(){
-        
-    }
-    
-}
---
+++ patterns/yogesh.java
@@ -0,0 +1,5 @@
+class yogesh{
+     public static void main(String[] args){
+	System.out.println("yogesh-joga_darling");
+	}
+ }
