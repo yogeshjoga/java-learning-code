d282c0d21b49a9c31616ef207e001c9bd0337760
yogesh joga <yjoga27@gmail.com>
2021-11-24 19:38:12 +0530

    adding

---
+++ Projects/My1stProject.java
@@ -0,0 +1,4 @@
+class My1stProject{
+    static int a,b,c,d;
+    static int checkWin;
+    static String[] square = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25"};
---
+++ Projects/adding.java
@@ -0,0 +1,8 @@
+public class adding {
+    public static void main(String[] yogi){
+        int a =10;
+        int b =20;
+        System.out.println(-(-a-(b)));
+
+    }
+}
