420e1647cc46951ba5a990ff46514d93c1bf7d64
yogesh joga <yjoga27@gmail.com>
2021-11-25 19:54:55 +0530

    arrays

---
+++ arrays/Currency.java
@@ -0,0 +1,38 @@
+//array is succesful dear
+public class Currency {
+   static int[] price = {100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500};
+
+    static void iCurrency(){
+        System.out.println();
+        System.out.println("=============funny currency================");
+        System.out.println();
+        System.out.println("indian rupee is : " +price[0]);
+        System.out.println("srikakulam rupee is : " +price[1]);
+        System.out.println("vizag rupee is : " +price[2]);
+        System.out.println("hyd rupee is : " +price[3]);
+        System.out.println("chennai rupee is : " +price[4]);
+        System.out.println("mumbai rupee is : " +price[5]);
+        System.out.println("bumbai rupee is : " +price[6]);
+        System.out.println("noida rupee is : " +price[7]);
+        System.out.println("thirupathi rupee is : " +price[8]);
+        System.out.println("telengana rupee is : " +price[9]);
+        System.out.println("palasa rupee is : " +price[10]);
+        System.out.println("pkd rupee is : " +price[11]);
+        System.out.println("cutm rupee is : " +price[12]);
+        System.out.println("baba rupee is : " +price[13]);
+        System.out.println("btc rupee is : " +price[14]);
+        System.out.println();
+        System.out.println("================Have a nice Day================");
+        System.out.println();
+        System.out.println("indian rupee is : " +price.length);
+        System.out.println();
+        System.out.println();
+
+
+    }
+
+    public static void main(String[] yogi){
+        iCurrency();
+    }
+    
+}
