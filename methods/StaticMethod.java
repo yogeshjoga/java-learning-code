505357ad969fb1fd2d64864a7a4804b0c2a7529c
yogesh joga <yjoga27@gmail.com>
2021-11-28 23:12:08 +0530

    ygoi

---
+++ methods/StaticMethod.java
@@ -0,0 +1,28 @@
+
+
+
+class StaticMethod{
+
+    //static method type double is the return type
+    static double sum(double a,float b){
+        return a+b;
+
+    }
+    //instance method type float is return type
+    float num(float a, float b){
+        return a+b;
+
+
+    }
+
+    public static void main (String[] args){
+        //create object to instance method it's not invocation to directly 
+        double cc = (a,b) 50.2, 42.222;
+        num ci = new num;
+        ci = "hello"
+
+
+
+
+    }
+}
