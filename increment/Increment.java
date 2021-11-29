3bd90957e99c21515295ff1a1ee4977dd8881c61
yogesh joga <yjoga27@gmail.com>
2021-11-29 23:37:55 +0530

    increment

---
+++ increment/Increment.java
@@ -0,0 +1,17 @@
+public class Increment {
+       /* int a=10,b=20
+        a++;
+        ++a;
+        b++;
+        ++b; */ 
+        public static void main(String[] args){
+            int a=10,b=20;
+            ++a;
+            a++;
+            //--a;
+          //  a--;
+            System.out.println(a);
+            System.out.println(b);
+        }
+    
+}
