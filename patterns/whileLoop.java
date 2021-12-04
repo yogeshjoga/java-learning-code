f0842768945a1c161a0753380bd9ca5f8ff319de
yogesh joga <yjoga27@gmail.com>
2021-12-04 22:11:33 +0530

    pattern

---
+++ patterns/whileLoop.java
@@ -0,0 +1,18 @@
+public class whileLoop {
+    public static void main(String[] args){
+        int a=15,b=1,c=1;
+
+        while(b<=a){
+            while(c<=b){
+                System.out.print("* ");
+                c++;
+            }
+           System.out.println();
+            b++;
+           // c = 1;
+        
+        }
+       // System.out.println();
+    }
+    
+}
