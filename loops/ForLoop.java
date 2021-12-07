169ae84ac723a68da28dade08b03d4dfc47b31da
yogesh joga <yjoga27@gmail.com>
2021-12-07 08:34:21 +0530

    loops

---
+++ loops/ForLoop.java
@@ -0,0 +1,17 @@
+public class ForLoop {
+    //static System.out.println();
+
+    static void aToZ(){
+        for( char ch='a'; ch<='z';ch++){
+            System.out.println(ch +"\n");
+        }
+    }
+   public static void main(String[] yogi){
+       aToZ();
+       for(int a=1; a<=100;a++){
+           System.out.println(a);
+        }
+   }
+
+    
+}
