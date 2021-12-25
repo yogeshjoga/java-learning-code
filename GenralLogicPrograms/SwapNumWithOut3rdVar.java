f7e594f7a986afc083ad1326e1e9ecb7f3c5aa6a
yogesh joga <yjoga27@gmail.com>
2021-12-26 00:13:00 +0530

    genral programming

--- GenralLogicPrograms/SwapNumWithOut3rdVar.java
+++ GenralLogicPrograms/SwapNumWithOut3rdVar.java
@@ -1,3 +1,11 @@
 public class SwapNumWithOut3rdVar {
+    public static void main(String[] args) {
+        int a=50,b=100,c=a,d=b;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+      //  System.out.println(a + "  "+ b);
+        System.out.println("\n A is : "+c+", is swaped into "+a +" &&  B is : "+d+", is swaped into  "+ b+"\n");
+    }
     
 }
--- GenralLogicPrograms/SwapTwoNumbers.java
+++ GenralLogicPrograms/SwapTwoNumbers.java
@@ -1,3 +1,10 @@
 public class SwapTwoNumbers {
+    public static void main(String[] args) {
+    int a=10,b=20,c,d=a,e=b;
+    c=a;
+    a=b;
+    b=c;
+    System.out.println("\n A is : "+d+", is swaped into "+a +" &&  B is : "+e+", is swaped into  "+ b+"\n");
+    }
     
 }
