f644880ab96d834f59cfda03407d995aff014f21
yogesh joga <yjoga27@gmail.com>
2021-12-27 23:31:47 +0530

    answers

--- GenralLogicPrograms/Factorial.java
+++ GenralLogicPrograms/Factorial.java
@@ -1,3 +1,12 @@
 public class Factorial {
+    public static void main(String[] args) {
+        long a=20, b=1;
+        for(int c=1; c<=a;c++){
+            b=b*c;
+            // for every itration you need to print u can uncomment the below statment
+           // System.out.println("the factroial of "+a+" is "+b+" \n");
+        }
+        System.out.println("\n The factroial of "+a+" is "+b+" \n");
+    }
     
 }
--- GenralLogicPrograms/FibonacciSeries.java
+++ GenralLogicPrograms/FibonacciSeries.java
@@ -1,3 +1,14 @@
 public class FibonacciSeries {
-    
+    public static void main(String[] args) {
+     int a=0,b=1,c;
+     while(true){
+        c=a+b;
+       }
+       if()
+    }
 }
+
+
+
+
+
