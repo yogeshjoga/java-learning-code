6ffd30ca7cba5e07d06b9807628331cc946660f7
yogesh joga <yjoga27@gmail.com>
2022-01-05 22:04:41 +0530

    project re-editing1

--- Projects/DmartBillingApp.java
+++ Projects/DmartBillingApp.java
@@ -4,7 +4,50 @@ import java.time.format.DateTimeFormatter;
 import java.lang.Math;
 import java.text.Format;
 public class DmartBillingApp {
-    static DmartBillingApp obj = new DmartBillingApp(); // static object
+       //Brands for shirts
+       String brand1 = "U.S.Polo";
+       String brand2 = "PUMA";
+       String brand3 = "Highlander";
+       String brand4 = "Roadstar";
+       String brand5 = "Nike";
+       // product price Tag's
+       int priceTag1 =1999;
+       int priceTag2 =2999;
+       int priceTag3 =999;
+       int priceTag4 =1299;
+       int priceTag5 =4999;
+       // product ID's
+       int id1 = 102051;
+       int id2 = 400052;
+       int id3 = 710153;
+       int id4 = 669954;
+       int id5 = 102055;
+       // jeans pants brands
+       String brandP1 = "U.S.Polo jeans";
+       String brandP2 = "PUMA jeans";
+       String brandP3 = "Highlander jeans";
+       String brandP4 = "Roadstar jeans";
+       String brandP5 = "Nike jeans";
+       // product price tag for pants
+       int priceTagP1 =3999;
+       int priceTagP2 =1999;
+       int priceTagP3 =1999;
+       int priceTagP4 =1399;
+       int priceTagP5 =2399; 
+       // product ID's
+       int idP1 = 902061;
+       int idP2 = 600062;
+       int idP3 = 710163;
+       int idP4 = 369964;
+       int idP5 = 502065;
+
+
+       ///
+       int a;
+       long totalPrice;
+       static DmartBillingApp obj = new DmartBillingApp(); // static object
+
+
     void comnyNam(){
         System.out.println("\n                       Dmart Shoppping                  ");
         System.out.println("----------------------------------------------------------");
@@ -22,6 +65,8 @@ public class DmartBillingApp {
         System.out.println("________________________Phone : 040-69014700______________");
         System.out.println("----------------------------------------------------------");  
     }
+
+
     void invoice(){
         int str =50;
         long min=100000;
@@ -42,52 +87,38 @@ public class DmartBillingApp {
         System.out.println("                         TAX INVOICE                     ");
         System.out.println("Bill No :"+str+""+random_int+""+(char)random_char+"-"+random_int2+"       "+"Bill Dt : "+timeNow);
         System.out.println("Vou.No : "+random_char1+""+random_int2+"-"+random_int+"        "+"Cashier : SBA/143143");
-        System.out.println("----------------------------------------------------------");
-        System.out.println(" HSN     Particulars     Qty/kgs      N/Rate       Value  ");
-        System.out.println("----------------------------------------------------------");
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println(" HSN     Particulars          Qty/kgs       N/Rate      Value          ");
+        System.out.println("-----------------------------------------------------------------------\n");
+        
+        System.out.println(id1+"   "+brand1+"                "+obj.a+"           "+obj.priceTag1+"         "+obj.totalPrice);
+        System.out.println(id2+"   "+brand2+"                    "+obj.a+"           "+obj.priceTag2+"         "+obj.totalPrice);
+        System.out.println(id3+"   "+brand3+"              "+obj.a+"           "+obj.priceTag3+"          "+obj.totalPrice);
+        System.out.println(id4+"   "+brand4+"                "+obj.a+"           "+obj.priceTag4+"         "+obj.totalPrice);
+        System.out.println(id5+"   "+brand5+"                    "+obj.a+"           "+obj.priceTag5+"         "+obj.totalPrice);
+        System.out.println(idP1+"   "+brandP1+"          "+obj.a+"           "+obj.priceTagP1+"         "+obj.totalPrice);
+        System.out.println(idP2+"   "+brandP2+"              "+obj.a+"           "+obj.priceTagP2+"         "+obj.totalPrice);
+        System.out.println(idP3+"   "+brandP3+"        "+obj.a+"           "+obj.priceTagP3+"         "+obj.totalPrice);
+        System.out.println(idP4+"   "+brandP4+"          "+obj.a+"           "+obj.priceTagP4+"         "+obj.totalPrice);
+        System.out.println(idP5+"   "+brandP5+"              "+obj.a+"           "+obj.priceTagP5+"         "+obj.totalPrice);
+        System.out.println("-----------------------------------------------------------------------\n");
+
+
+        System.out.println("__________________________________________________________________________");
+        System.out.println("__________________________________________________________________________\n \n");
+        
+
+
     }
 
+
+   
+
     void products(){
-        //Brands for shirts
-        String brand1 = "U.S.Polo";
-        String brand2 = "PUMA";
-        String brand3 = "Highlander";
-        String brand4 = "Roadstar";
-        String brand5 = "Nike";
-        // product price Tag's
-        int priceTag1 =1999;
-        int priceTag2 =2999;
-        int priceTag3 =999;
-        int priceTag4 =1299;
-        int priceTag5 =4999;
-        // product ID's
-        int id1 = 102051;
-        int id2 = 400052;
-        int id3 = 710153;
-        int id4 = 669954;
-        int id5 = 102055;
-        // jeans pants brands
-        String brandP1 = "U.S.Polo jeans";
-        String brandP2 = "PUMA jeans";
-        String brandP3 = "Highlander jeans";
-        String brandP4 = "Roadstar jeans";
-        String brandP5 = "Nike jeans";
-        // product price tag for pants
-        int priceTagP1 =3999;
-        int priceTagP2 =1999;
-        int priceTagP3 =1999;
-        int priceTagP4 =1399;
-        int priceTagP5 =2399; 
-        // product ID's
-        int idP1 = 902061;
-        int idP2 = 600062;
-        int idP3 = 710163;
-        int idP4 = 369964;
-        int idP5 = 502065;
         System.out.println("\n----------------------------------------------------------");
         System.out.println("                   WELCOME TO DMART                         ");
         System.out.println("               SELECT THE YOUR PRODUCTS                     ");
-        System.out.println("----------------------------------------------------------");
+        System.out.println("------------------------------------------------------------");
         String aa = "U.S.Polo";
         String bb = "PUMA";
         String cc = "Highlander";
@@ -139,28 +170,24 @@ public class DmartBillingApp {
         }
         // total price of prodcts
         Scanner quantity = new Scanner(System.in);
-        int a = quantity.nextInt();
-        long totalPrice = a*priceTag1;
+        obj.a = quantity.nextInt();
+        obj.totalPrice = obj.a*obj.priceTag1;
         // now printing the values
-	    //System.out.println("----------------------------------------------------------");
-        
-
-        System.out.println(id1+"   "+brand1+"                "+a+"           "+priceTag1+"         "+totalPrice);
-        System.out.println(id2+"   "+brand2+"                    "+a+"           "+priceTag2+"         "+totalPrice);
-        System.out.println(id3+"   "+brand3+"              "+a+"           "+priceTag3+"          "+totalPrice);
-        System.out.println(id4+"   "+brand4+"                "+a+"           "+priceTag4+"         "+totalPrice);
-        System.out.println(id5+"   "+brand5+"                    "+a+"           "+priceTag5+"         "+totalPrice);
-        System.out.println(idP1+"   "+brandP1+"          "+a+"           "+priceTagP1+"         "+totalPrice);
-        System.out.println(idP2+"   "+brandP2+"              "+a+"           "+priceTagP2+"         "+totalPrice);
-        System.out.println(idP3+"   "+brandP3+"        "+a+"           "+priceTagP3+"         "+totalPrice);
-        System.out.println(idP4+"   "+brandP4+"          "+a+"           "+priceTagP4+"         "+totalPrice);
-        System.out.println(idP5+"   "+brandP5+"              "+a+"           "+priceTagP5+"         "+totalPrice);
-
+	    // System.out.println("----------------------------------------------------------");
+    }
+    
+    void gstBreakUp(){
+        System.out.println("<---------------GST BreakUp Details-------------->(Amount INR)\n");
+        System.out.println("------------------------------------------------------------");
+        System.out.println("GST     Taxable       CGST     SGST      CESS          Total ");
+        System.out.println("IND     Amount                                         Amount");
+        System.out.println("------------------------------------------------------------");
 
     }
 
     void shopping(){
         Scanner shop = new Scanner(System.in);
+
         // shirtShopping = shop.next(), pantShopping =shop.next(),shirtQuantity = shop.next(), pantQuantity = shop.next();
     }
     public static void main(String[] args) {
@@ -168,10 +195,7 @@ public class DmartBillingApp {
         bill.products();
         bill.comnyNam();
         bill.invoice();
-     // bill.shoppingItems();
-	 // bill.products();
-     // bill.shirts();
-     // bill.pants();
-     // bill.invoice();
+        bill.gstBreakUp();
+  
     }
 }
