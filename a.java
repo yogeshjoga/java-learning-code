5df12ac9fe112e99d24e6f252cd7f3feee0a9614
yogesh joga <yjoga27@gmail.com>
2022-01-08 14:46:03 +0530

    floating point

---
+++ a.java
@@ -0,0 +1,19 @@
+public class a {
+    public static void main(String[] args) {
+        
+    double b = 3.4;
+    float a=3.4f;
+    if (a==3.4){  // it is not equal to the actual value y bcoz float values are taken size of floating points
+        System.out.println("ok");
+    }
+    else{
+        System.out.println("cancle");
+    }
+    if (b==3.4){
+        System.out.println("double is ok");
+    }
+    else{
+        System.out.println("doulble is cancle");
+    } 
+ }   
+}
