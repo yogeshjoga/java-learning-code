73586399c2729284f62438e75f09255062f18af3
yogesh joga <yjoga27@gmail.com>
2022-01-14 23:47:59 +0530

    code

--- Logical100Questions/FriendlyNumber.java
+++ Logical100Questions/FriendlyNumber.java
@@ -1,3 +1,9 @@
+import java.util.Scanner; 
+
 public class FriendlyNumber {
-    
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int num1 = num.nextInt();
+        
+    }
 }
