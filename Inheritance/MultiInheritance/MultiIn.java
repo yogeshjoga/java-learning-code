da363da6797175f93961700c60c31a6faa1b1b7b
yogesh joga <yjoga27@gmail.com>
2022-01-17 13:16:14 +0530

    Inhertance Comments

--- Inheritance/MultiInheritance/MultiIn.java
+++ Inheritance/MultiInheritance/MultiIn.java
@@ -1,13 +1,13 @@
 // Multi Inhertance progeramming 
 class Animals{
-    void behivor(){
+    void behivor(){  //Super class 
         System.out.println("it as having four legs");
         System.out.println(" it is walking and running");
         System.out.println("crying and laughing");
         System.out.println("jumping ");
     }
 }
-class Dogs extends Animals{
+class Dogs extends Animals{ // 1st child class to Animal class
     Animals dogg = new Animals();
     void dogsBow(){
         System.out.println("i have these behivors ");
@@ -15,18 +15,18 @@ class Dogs extends Animals{
         System.out.println("i am Brakking like Boww Boww");
     }
 }
-class GermanSherphed1 extends Dogs{
+class GermanSherphed1 extends Dogs{ // child calss to Dogs class
     void germanShe(){
         System.out.println(" im a German Sherphed");
         Dogs ddd = new Dogs();
         ddd.dogsBow();
     } 
 }
-public class MultiIn {
+public class MultiIn { // maiin class
     static void disply(){
         System.out.println("WelCome This is the Multi Inhertance...");
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) { // main method 
         disply();
         GermanSherphed1 ger = new GermanSherphed1();
         ger.germanShe();
