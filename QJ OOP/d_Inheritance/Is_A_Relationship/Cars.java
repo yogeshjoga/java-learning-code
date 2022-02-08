0a18ea6124715516f6d5bbd5c76beb2dbc947392
yogesh joga <yjoga27@gmail.com>
2022-02-08 23:24:22 +0530

    updatess

--- QJ OOP/d_Inheritance/Is_A_Relationship/Cars.java
+++ QJ OOP/d_Inheritance/Is_A_Relationship/Cars.java
@@ -1,18 +1,55 @@
+import java.util.Scanner;
+
 public class Cars {
-    public int carWheels;
-    public int carSeats;
-    public int carDoors;
-    public int carWe(int carWheels){
-        this.carWheels = carWheels;
-        return this.carWheels;
+    public boolean carWheels;
+    public boolean carSeats;
+    public boolean carDoors;
+    Scanner sc = new Scanner(System.in);
+
+    public boolean carWe(boolean carWheels){
+        if(carWheels==true){
+            int noOfWheels = this.sc.nextInt();
+            switch(noOfWheels){
+                case 1: System.out.println("\n4 Wheels 2 Wheel Drive"+this.carWheels);
+                break;
+                case 2: System.out.println("\n4 Wheels 4 Wheel Drive"+this.carWheels);
+                break;
+                case 3: System.out.println("\n6 Wheels 4 Wheel Drive"+this.carWheels);
+                break;
+                case 4: System.out.println("\n6 Wheels 6 Wheel Drive"+this.carWheels);
+                break;
+              }
+            
+        }
+        return true;
     }
-    public int carSe(int carSeats){
-        this.carSeats = carSeats;
-        return this.carSeats;
+
+    public boolean carSe(boolean carSeats){
+        if(carSeats==true){
+            int noOfCarSeat = this.sc.nextInt();
+            switch(noOfCarSeat){
+                case 1: System.out.println("\n5 Sitting 5 seats in-cluding driver seat");
+                break;
+                case 2: System.out.println("\n7 Sitting 7 seats in-cluding driver seat");
+                break;
+            }
+        }
+        return true;
     }
-    public int carDo(int carDoors){
-        this.carDoors = carDoors;
-        return this.carDoors;
+
+    public boolean carDo(boolean carDoors){
+        if(carDoors==true){
+         this.carDoors = carDoors;
+         int noOfCarDoor = this.sc.nextInt();
+          switch(noOfCarDoor){
+              case 1: System.out.println("\n3 door car without sun-roof");
+              case 2: System.out.println("\n5 doors car without sun-roof");
+              break;
+              case 3: System.out.println("\nNo doors open off-Road vehicle");
+              break;
+             }
+         }
+         return true;
     }
     
 }
--- QJ OOP/d_Inheritance/Is_A_Relationship/CarsParts.java
+++ QJ OOP/d_Inheritance/Is_A_Relationship/CarsParts.java
@@ -1,26 +1,43 @@
 import java.util.Scanner;
 
 public class CarsParts extends Cars {
+    Scanner sc = new Scanner(System.in);
+    CarsParts obj = new CarsParts();
     public void engine(){
         System.out.println("\nThe engine specefictions is the given below : ");
-        Scanner sc = new Scanner(System.in);
         System.out.println("\n\tTypes of engines\n1) 2 Cylinder 200mm v2 2 liter turbo engine \n2) 2 Cylinder 400mm v2 4 liter turbo engine");
         System.out.println("3) 4 Cylinder 400mm v3 4 liter turbo engine \n4) 4 Cylinder 600mm v3 4 liter turbo engine");
-        int engineTypes = sc.nextInt();
+        int engineTypes = this.sc.nextInt();
         switch(engineTypes){
-            case 1 : System.out.println("2 Cylinder 200mm v2 2 liter turbo engine");
+            case 1 : System.out.println("Your car is 2 Cylinder 200mm v2 2 liter turbo engine");
             break;
-            case 2 : System.out.println("2 Cylinder 400mm v2 4 liter turbo engine");
+            case 2 : System.out.println("Your car is 2 Cylinder 400mm v2 4 liter turbo engine");
             break;
-            case 3: System.out.println("4 Cylinder 400mm v3 4 liter turbo engine");
+            case 3: System.out.println("Your car is 4 Cylinder 400mm v3 4 liter turbo engine");
             break;
-            case 4: System.out.println(" 4 Cylinder 600mm v3 4 liter turbo engine");
+            case 4: System.out.println("Your car is 4 Cylinder 600mm v3 4 liter turbo engine");
             break;
             default : System.out.println("choose the your engine type correctly ");
         }
     }
+    public void fuelType(){
+        System.out.println("\n1) Petrol Engine \n2) Diesel Engine");
+        int fuelType = this.sc.nextInt();
+        switch(fuelType){
+            case 1: System.out.println("Your car fuel Type is Petrol Engine");
+            break;
+            case 2: System.out.println("Your car fuel Type is Diesel Engine");
+            break;
+        }
+    }
+    
+
     public static void main(String[] args) {
         CarsParts obj = new CarsParts();
         obj.engine();
+        obj.fuelType();
+        obj.carWe(true);
+        obj.carSe(true);
+        obj.carDo(true);
     }
 }
