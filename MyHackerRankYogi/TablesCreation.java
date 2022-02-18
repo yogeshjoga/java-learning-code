b588456ebc6c83353e79f2c1d02ddfb038392db5
yogesh joga <yjoga27@gmail.com>
2022-02-18 23:10:50 +0530

    code

---
+++ MyHackerRankYogi/TablesCreation.java
@@ -0,0 +1,22 @@
+import java.util.scanner;
+public class TablesCreation {
+    Scanner sc = new Scanner(System.in);
+    public int tableOutput(int whichTable){
+        int mult;
+        for(int i =1; i <=10; i++){
+            mult = whichTable * i;
+            System.out.println(whichTalbe+""+i+""+mult);
+           
+        }
+      return mult;
+    }
+    public static void mian(String[] args){
+        TablesCreation obj = new TablesCreation();
+        int inputTable = obj.sc.nextInt();
+        obj.tableOutput(inputTable);
+
+        
+    }
+    
+    
+}
