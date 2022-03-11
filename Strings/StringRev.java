ce98ecb3059b3190140ef962335a6a8e0530b3d7
yogesh joga <yjoga27@gmail.com>
2022-03-11 13:03:52 +0530

    strings questions slove

--- Strings/StringRev.java
+++ Strings/StringRev.java
@@ -52,19 +52,89 @@ public class StringRev {
      * write a programming to finding indexOf method values fo a given char in all its
      * occeraneces fo a given string
      */
-    public void indexOfFunction() throws Exception{
-        System.out.println("Enter the string ");
+    public static void indexOfFunction() throws Exception{
+        System.out.println(" Enter the string ");
         String s = StringRev.sc.nextLine();
-
+        System.out.println(" Enter a char to find given string indexOf ");
+        String s1 = StringRev.sc.next();
+        int ind = 0;
         for(int i=0; i<s.length();i++){
-
+            ind = s.indexOf(s1,i);
+            System.out.println(ind);
         }
     }
 
+
+    /**
+     * 
+     * @param args
+     * @throws Exception
+     * write a programming to remover the duplicate char's presented in the 
+     * given string 
+     *  1st quetion is using indexOf method to solve this 
+     *  2nd question is without using indexOf method to slove this 
+     * 
+     */
+    // by using indexOf method to remove the chars presented in string
+    public static void remCharIndex(){
+        System.out.println(" Enter a line of String to remove the duplicate chars ");
+        String s = StringRev.sc.nextLine();
+        String s1="";
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(s1.indexOf(ch)<0){
+                s1=s1+ch; // s1+=ch we can write this manner also
+            }
+        }
+        System.out.println(s1);
+
+    }
+    /**
+     * 
+     * @param args
+     * @throws Exception
+     * with out using indexOf method to write a programming to remvoe the duplicate chars
+     * in the string values
+     * thats it
+     */
+
+    public static void arrayRemoveDupliacte(){
+
+    }
+    
+    /**
+     * 
+     * @param args
+     * @throws Exception
+     *  write a programm to add all the digits preseted in the given mixed string
+     *  in integer formate only?
+     * 
+     */
+    public static void stringsToIntegers(){
+        System.out.println(" Enter a string with number to add the all numric values from given string ");
+        String s = StringRev.sc.nextLine();
+        int add = 0;
+        for(int i=0; i<s.length();i++){
+            // we can use wapper classes 
+            if(Character.isDigit(s.charAt(i))){
+            add = add+Character.getNumericValue(s.charAt(i));
+            }
+          //  System.out.println(add);
+        }
+        System.out.println(s+" the string containing these numbers "+add);
+    }
+
+
+
+
+
     // user logic of the main method
     public static void main(String[] args) throws Exception {
-        stringRevers();
-        stringFindingVowels();
+       // stringRevers();
+       // stringFindingVowels();
+      // indexOfFunction();
+      // remCharIndex();
+      stringsToIntegers();
     }
     
 
