e9f9fc6d07cdafd510cddc39dfa7417d4f139a06
yogesh joga <yjoga27@gmail.com>
2022-03-18 10:34:14 +0530

    ygoesh

--- object_class/StaticOject.java
+++ object_class/StaticOject.java
@@ -4,9 +4,4 @@ public class StaticObject {
     static Scanner sc = new Scanner(System.in);
     int a, b;
 
-    public void add(String name) {
-        syso
-        if(char ch='0'&& ch='9'){}
-
-    }
 }
