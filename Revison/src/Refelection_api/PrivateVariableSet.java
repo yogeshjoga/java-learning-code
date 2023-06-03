f929a37369de02af226fb26cd0862f3ed7cf23f9
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-06-04 00:13:10 +0530

    reflection apis

--- Revison/src/Refelection_api/PrivateVariableSet.java
+++ Revison/src/Refelection_api/PrivateVariableSet.java
@@ -1,15 +1,43 @@
 package Refelection_api;
 
+import java.lang.reflect.Field;
+
 public class PrivateVariableSet {
 
-    private final String myName = "yogesh";
+    private final String myName;
+
+    public PrivateVariableSet(String myName) {
+        this.myName = myName;
+    }
+
+    public  String getMyName(){
+        return myName;
+    }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         System.out.println("default --------------- yogesh");
 
         /**
          * reflection api used to and manipulate the final variable
          */
+
+
+        PrivateVariableSet pt = new PrivateVariableSet("yogesh");
+        // printing the object values
+        System.out.println(pt.getMyName());
+
+        Field [] fl = pt.getClass().getDeclaredFields();
+       // fl.forEach(n -> System.out.println(n));
+        for(Field f: fl){
+            if(f.getName().equals("myName")){
+                System.out.println("Updated");
+                f.setAccessible(true);
+                f.set(pt,"Hello billa I am hack your system");
+
+            }
+        }
+        // after updating the variable printing
+        System.out.println(pt.getMyName());
     }
 }
