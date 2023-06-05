77e6d1be9f694ba6fe07d4962b0c567710d3296f
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-06-06 00:05:01 +0530

    threading and arraylist vector

--- Revison/src/Collections/ArrayList001.java
+++ Revison/src/Collections/ArrayList001.java
@@ -1,6 +1,6 @@
 package Collections;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class ArrayList001 {
@@ -14,19 +14,43 @@ public class ArrayList001 {
      */
 
     // ArrayList excution time
-    public void ArrayListExcution(int l){
+    public static void ArrayListExcution(int l){
         ArrayList<Integer> list = new ArrayList<>();
+
+        // Staring time
+        long start = System.currentTimeMillis();
         Thread t1 = new Thread(() -> {
-           Stream.iterate(1, i -> i<=l, i -> i++);
-            list.add();
+          // Stream.iterate(1, i -> i<=l, i -> i++);
+           for(int i=0; i<=l; i++){
+               list.add(i);
+           }
         });
+        long end  = System.currentTimeMillis();
+        System.out.println("time = "+ (start-end));
 
     }
+
+    // syncronized list
+     public static void sycnArraylist(){
+         // non-syncronized
+        List<Integer> li = new ArrayList<>();
+        // syncronized List
+         List<Integer> lis = Collections.synchronizedList(new ArrayList<>());
+         Thread t1 = new Thread(() -> {
+             for(int i=0; i<200000; i++){
+                 lis.add(i);
+             }
+          });
+
+         // collections ended
+
+     }
 
 
 
 
     public static void main(String[] args) {
+        ArrayListExcution(2000000);
 
     }
 }
