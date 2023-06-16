6a46076ea4c1170f9211e4473a7ba006a488fb7b
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-06-16 22:55:31 +0530

    Create Base64Hash.java

---
+++ Revison/src/Java8Fet_Rev/EncodeaAndDecode/Base64Hash.java
@@ -0,0 +1,19 @@
+package Java8Fet_Rev.EncodeaAndDecode;
+
+/**
+ *
+ * @since <b>Java 8v feature</b>
+ * @see java.util.Base64
+ *
+ * @author <b>Yogesh Joga</b>
+ */
+public class Base64Hash {
+
+    /**
+     * <p>Base64 hashing is the java 8v feature</p>
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
