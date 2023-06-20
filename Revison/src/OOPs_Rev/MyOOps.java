6b980d8d93684515edc207188d1cf4246042c672
yogeshjoga <yjoga27@gmail.com>
2023-06-20 23:54:27 +0530

    updated

--- Revison/src/OOPs_Rev/MyOOps.java
+++ Revison/src/OOPs_Rev/MyOOps.java
@@ -3,7 +3,22 @@ package OOPs_Rev;
 
 /**
  *
+ * <p>
+ *     <b>OOP's have six concepts, The major four OOP concepts is  Given list </b>
+ *     <ol>
+ *         <li><b>Object</b></li>
+ *         <li><b>Class</b></li>
+ *         <b>The Major Four concepts are</b>
+ *         <li><b>Polymorphism</b></li>
+ *         <li><b>Abstraction</b></li>
+ *         <li><b>Inheritance</b></li>
+ *         <li><b>Encapsulation</b></li>
+ *
+ *     </ol>
+ * </p>
  * @author <b>Yogesh Joga</b>
  */
 public class MyOOps {
+
+
 }
