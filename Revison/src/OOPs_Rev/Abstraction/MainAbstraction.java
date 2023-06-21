e75071b5b129dfecddeb3723d87e042a99703242
yogeshjoga <yjoga27@gmail.com>
2023-06-21 23:04:12 +0530

    oops

--- Revison/src/OOPs_Rev/Abstraction/MainAbstraction.java
+++ Revison/src/OOPs_Rev/Abstraction/MainAbstraction.java
@@ -5,4 +5,6 @@ package OOPs_Rev.Abstraction;
  * @author <b>Yogesh Joga</b>
  */
 public interface MainAbstraction {
+
+
 }
--- Revison/src/OOPs_Rev/Object/MainObj.java
+++ Revison/src/OOPs_Rev/Object/MainObj.java
@@ -1,7 +1,16 @@
 package OOPs_Rev.Object;
 
+import com.sun.tools.javac.Main;
+
 /**
  * @author <b>Yogesh Joga</b>
  */
 public class MainObj {
+
+
+    public static void main(String[] args) {
+        // this is called object
+        MainObj mainObj = new MainObj();
+
+    }
 }
