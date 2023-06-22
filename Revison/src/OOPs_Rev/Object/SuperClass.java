c2dd2ee5b7fb4ee134de4aec7aa3c1cedf6a2e27
yogeshjoga <yjoga27@gmail.com>
2023-06-22 23:51:49 +0530

    oops

---
+++ Revison/src/OOPs_Rev/Object/SuperClass.java
@@ -0,0 +1,21 @@
+package OOPs_Rev.Object;
+
+/**
+ *
+ *
+ * @author <b>Yogesh Joga</b>
+ */
+public class SuperClass {
+
+    String myName;
+    int addNum;
+
+    public void hello(){
+        System.out.println("hello this is from super class method");
+    }
+
+    public static void hellStatic(){
+        System.out.println("helloStatic this is from super class method");
+    }
+
+}
