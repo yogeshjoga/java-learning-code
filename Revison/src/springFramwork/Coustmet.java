4d64d5baf5184b7c7352db5cbc30f566013043c1
yogeshjoga <yjoga27@gmail.com>
2023-06-26 00:07:54 +0530

    spring ioc

--- Revison/src/springFramwork/Coustmet.java
+++
@@ -1,5 +0,0 @@
-package springFramwork;
-
-public class Coustmet {
-
-}
--- Revison/src/springFramwork/Mobile.java
+++ Revison/src/springFramwork/Mobile.java
@@ -8,11 +8,28 @@ package springFramwork;
 public class Mobile {
 
     public static void main(String[] args) {
+
+        // with-out spring
+
+
         // created a mobile class that's run the sim features from sim interface
         Sim sim = new Airtel(); // before sim we can create one one object to every sim card as well as mobile also
         // calling feature from sim
         sim.calling();
         // internet feature from sim
         sim.internet();
+
+
+
+        // with spring
+        // lets run it and configrure to spring module
+        // crate spring project and run this code
+
+      /*  ApplicationContext context = new ClassPathXmlApplicationContext( "web.xml");
+        Sim simSp =  context.getBean("sim", Sim.class);
+        simSp.calling();
+        simSp.internet();
+
+       */
     }
 }
